8c11f35495b71263cfc338243d0d6378d1e52dac
abdulrahmanOmarr <2xr,eng@gmail.com>
2025-03-06 01:29:50 +0300

    update in user seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -10,20 +10,55 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('users')->insert([
-            'first_name' => 'أحمد',
-            'last_name' => 'المالكي',
-            'username' => 'ahmad99',
-            'email' => 'ahmad@example.com',
-            'password' => Hash::make('password123'),
-            'birthdate' => '1997-02-20',
-            'phone' => '0569876543',
-            'sex' => 'Male',
-            'address' => 'جدة، السعودية',
-            'city' => 'جدة',
-            'state' => 'مكة',
-            'street' => 'التحلية',
-            'neighborhood' => 'السلامة',
-        ]);
+        $users = [
+            [
+                'first_name' => 'أحمد',
+                'last_name' => 'المالكي',
+                'username' => 'ahmad99',
+                'email' => 'ahmad@example.com',
+                'password' => Hash::make('password123'),
+                'birthdate' => '1997-02-20',
+                'phone' => '0569876543',
+                'sex' => 'Male',
+                'city' => 'جدة',
+                'state' => 'مكة',
+                'street' => 'التحلية',
+                'neighborhood' => 'السلامة',
+                
+            ],
+            [
+                'first_name' => 'سارة',
+                'last_name' => 'العمري',
+                'username' => 'sara88',
+                'email' => 'sara@example.com',
+                'password' => Hash::make('pass456'),
+                'birthdate' => '1995-05-15',
+                'phone' => '0551234567',
+                'sex' => 'Female',
+                'city' => 'الرياض',
+                'state' => 'الرياض',
+                'street' => 'الملك فهد',
+                'neighborhood' => 'العليا',
+                
+            ],
+            [
+                'first_name' => 'محمد',
+                'last_name' => 'الزهراني',
+                'username' => 'mohd77',
+                'email' => 'mohd@example.com',
+                'password' => Hash::make('mypass789'),
+                'birthdate' => '1990-11-10',
+                'phone' => '0547891234',
+                'sex' => 'Male',
+                'city' => 'مكة',
+                'state' => 'مكة',
+                'street' => 'الحجون',
+                'neighborhood' => 'الزاهر',
+                
+            ],
+        ];
+
+        // Insert all users into the database
+        DB::table('users')->insert($users);
     }
 }
